6072b082022f3d4a736c768e469a3aee52c55e8a
davi1304 <76175363+davi1304@users.noreply.github.com>
2020-12-28 11:53:32 -0300

    Parte da aplicação do projeto

---
+++ Restaurante.Aplicacao/MappingEntidade.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Restaurante.Aplicacao.DTO;
+using Restaurante.Dominio.Entidades;
+namespace Restaurante.Aplicacao
+{
+    public class MappingEntidade : Profile
+    {
+        public MappingEntidade()
+        {
+            CreateMap<Prato, PratoDTO>();
+            CreateMap<PratoDTO, Prato>();
+        }
+    }
+}
